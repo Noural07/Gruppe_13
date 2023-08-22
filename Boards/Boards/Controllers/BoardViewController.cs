29c04336b49c17c9b768f1792fe42e249dae2c68
007Muddy <101377307+007Muddy@users.noreply.github.com>
2023-08-22 11:38:05 +0200

    asd
    BoardView

---
+++ Boards/Boards/Controllers/BoardViewController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Boards.Models;
+
+namespace Boards.Controllers
+{
+    public class BoardViewController : Controller
+    {
+        public IActionResult Create()
+        {
+            Board board = new Board()
+            {
+
+                ID = 1,
+                Name = "Best Way Hydro",
+                Length = 6,
+                Width = 21,
+                Thickness = 2.75,
+                Volume = 38.8,
+                Type = "ShortBoard",
+                Price = 565,
+                Equipment = "",
+
+
+            };
+
+            return View(board);
+        }
+
+
+    }
+}
