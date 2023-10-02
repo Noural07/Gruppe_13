f06521ce66cad9875be965612e4e8c2f1d34eb5f
007Muddy <101377307+007Muddy@users.noreply.github.com>
2023-10-02 12:45:19 +0200

    1
    1

---
+++ Boards/Boards/Migrations/20231002100239_boardkeyadded.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boards.Migrations
+{
+    public partial class boardkeyadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
